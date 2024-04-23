da84ec41998344c8a39329efa67767aad6bd5b4d
DeveloperDave17 <DavidHennigan17@gmail.com>
2024-04-23 11:50:10 -0400

    add skeleton heartbeat functionality

--- src/main/java/edu/oswego/cs/raft/Raft.java
+++ src/main/java/edu/oswego/cs/raft/Raft.java
@@ -1,5 +1,35 @@
 package edu.oswego.cs.raft;
 
+import java.net.SocketAddress;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Raft {
    
+   private final Timer heartBeatTimer = new Timer();
+   private final ConcurrentHashMap<String, SocketAddress> sessionMap = new ConcurrentHashMap<>();
+
+   public void startHeartBeat() {
+      TimerTask task = new TimerTask() {
+         public void run() {
+            System.out.println("heartbeat");
+            sessionMap.forEachValue(Long.MAX_VALUE, (value) -> {
+               // send to socket address
+               System.out.println(value);
+            });
+         }
+      };
+
+      heartBeatTimer.schedule(task, 0, 100);
+   }
+
+   public void stopHeartBeat() {
+      heartBeatTimer.cancel();
+   }
+
+   public void addSession(String session, SocketAddress socketAddress) {
+      sessionMap.put(session, socketAddress);
+   }
 }
