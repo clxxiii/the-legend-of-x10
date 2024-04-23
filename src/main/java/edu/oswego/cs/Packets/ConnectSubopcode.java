0a9e7e81716ed7ad1d5aac97a25f77834201e4fd
Victor-Lockwood <vlockwoo@oswego.edu>
2024-04-23 19:15:50 -0400

    Packet stuff

---
+++ src/main/java/edu/oswego/cs/Packets/ConnectSubopcode.java
@@ -0,0 +1,26 @@
+package edu.oswego.cs.Packets;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public enum ConnectSubopcode {
+    ClientHello(1),
+    ServerHello(2),
+    ClientKey(3),
+    Log(4);
+
+    public final short code;
+
+    ConnectSubopcode(int i) {
+        code = (short) i;
+    }
+
+    /**
+     * Grab the associated ConnectSubopcode given a short.
+     * @param i Short value of the ConnectSubopcode.
+     * @return The associated ConnectSubopcode.
+     */
+    public static Optional<ConnectSubopcode> fromCode(short i) {
+        return Arrays.stream(values()).filter(x -> x.code == i).findFirst();
+    }
+}
---
+++ src/main/java/edu/oswego/cs/Packets/Packet.java
@@ -0,0 +1,45 @@
+package edu.oswego.cs.Packets;
+
+import java.nio.ByteBuffer;
+import java.text.ParseException;
+import java.util.Optional;
+
+//My name's gonna be on this, but Eli was a lot smarter than I was for
+//packet structures in project 2, so I'm going by that.
+//Comments are mine, however.
+public abstract class Packet {
+    public final Opcode opcode;
+
+    protected Packet(Opcode opcode) {
+        this.opcode = opcode;
+    }
+
+    public abstract byte[] packetToBytes();
+
+    public static Packet packetToBytes(ByteBuffer buffer) throws ParseException {
+        buffer.flip();
+
+        //Get the opcode short from the packet
+        short code = buffer.getShort();
+        Optional<Opcode> optionOpcode = Opcode.fromCode(code);
+
+        //Make sure something's actually there
+        if(!optionOpcode.isPresent()) {
+            throw new ParseException("Invalid opcode", 0);
+        }
+
+        //Now get the ACTUAL opcode
+        Opcode opcode = optionOpcode.get();
+
+        //This seems redundant considering we got the stuff earlier straight from the bytes,
+        //but that was necessary to validate we actually got a valid opcode.
+        //TODO: Fill this out once we get actual packet types
+        switch (opcode.code) {
+            case 1:
+                return ConnectPacket.bytesToPacket(buffer);
+            default:
+                return null;
+        }
+
+    }
+}
