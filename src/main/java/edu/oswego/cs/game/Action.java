7aea1f271f55c62131768650e0d4e6dcbb9e421c
DeveloperDave17 <DavidHennigan17@gmail.com>
2024-04-24 14:30:23 -0400

    add client username

--- src/main/java/edu/oswego/cs/game/Action.java
+++ src/main/java/edu/oswego/cs/game/Action.java
@@ -5,14 +5,20 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Action {
     private String command;
     private AtomicInteger numCommitted;
+    private String userName;
 
-    public Action(String command) {
+    public Action(String userName, String command) {
+        this.userName = userName;
         this.command = command;
         this.numCommitted = new AtomicInteger(0);
     }
 
     public String getCommand() {
         return command;
+    }
+
+    public String getUserName() {
+        return userName;
     }
 
     public int getNumCommited() {
