f2d21bddf150b1661f9cfff586c55d3026de1df6
DeveloperDave17 <DavidHennigan17@gmail.com>
2024-04-24 14:30:41 -0400

    create Session

---
+++ src/main/java/edu/oswego/cs/raft/Session.java
@@ -0,0 +1,38 @@
+package edu.oswego.cs.raft;
+
+import java.net.SocketAddress;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class Session {
+
+    private AtomicReference<SocketAddress> addressAtomicReference = new AtomicReference<>();
+    // Last Message Received Time Stamp In Nano Time
+    private AtomicLong LMRSTINT = new AtomicLong();
+    private AtomicReference<RaftMembershipState> raftMembershipStateAtomicReference = new AtomicReference<>();
+    private AtomicBoolean timedOut = new AtomicBoolean();
+
+    public Session(SocketAddress socketAddress, long LMRSTINT, RaftMembershipState raftMembershipState) {
+        this.addressAtomicReference.set(socketAddress);
+        this.LMRSTINT.set(LMRSTINT);
+        this.raftMembershipStateAtomicReference.set(raftMembershipState);
+        timedOut.set(true);
+    }
+
+    public SocketAddress getSocketAddress() {
+        return addressAtomicReference.get();
+    }
+
+    public long getLMRSTINT() {
+        return LMRSTINT.get();
+    }
+
+    public RaftMembershipState getMembershipState() {
+        return raftMembershipStateAtomicReference.get();
+    }
+
+    public boolean getTimedOut() {
+        return timedOut.get();
+    }
+}
