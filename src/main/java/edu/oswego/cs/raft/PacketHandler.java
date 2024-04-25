6f38aa5429fd8fa81f4e341c92fe111334ac2514
DeveloperDave17 <DavidHennigan17@gmail.com>
2024-04-24 20:57:06 -0400

    add connection and heartbeat packet handling

--- src/main/java/edu/oswego/cs/raft/PacketHandler.java
+++ src/main/java/edu/oswego/cs/raft/PacketHandler.java
@@ -2,7 +2,9 @@ package edu.oswego.cs.raft;
 
 import edu.oswego.cs.Packets.*;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.SocketAddress;
 import java.nio.ByteBuffer;
 import java.text.ParseException;
@@ -10,10 +12,14 @@ import java.text.ParseException;
 public class PacketHandler extends Thread {
     private final DatagramPacket packet;
     private final Raft raft;
+    private final String serverUsername;
+    private final DatagramSocket serverSocket;
 
-    public PacketHandler(DatagramPacket packet, Raft raft) {
+    public PacketHandler(DatagramPacket packet, Raft raft, String serverUsername, DatagramSocket serverSocket) {
         this.packet = packet;
         this.raft = raft;
+        this.serverUsername = serverUsername;
+        this.serverSocket = serverSocket;
     }
 
     @Override
@@ -23,7 +29,6 @@ public class PacketHandler extends Thread {
         byte[] packetData = packet.getData();
         ByteBuffer packetBuffer = ByteBuffer.allocate(packetData.length);
         packetBuffer.put(packetData);
-        packetBuffer.reset();
         try {
             Packet packet = Packet.bytesToPacket(packetBuffer);
             if (packet != null) {
@@ -52,38 +57,104 @@ public class PacketHandler extends Thread {
         }
     }
 
-    public void handleConnectPacket(Packet packet, SocketAddress clientAddr) {
+    public void handleConnectPacket(Packet packet, SocketAddress socketAddr) {
         ConnectPacket connectPacket = (ConnectPacket) packet;
         switch (connectPacket.subopcode) {
             case ClientHello:
-                handleClientHello(connectPacket, clientAddr);
+                handleClientHello(connectPacket, socketAddr);
                 break;
             case ServerHello:
+                handleServerHello(connectPacket, socketAddr);
                 break;
             case ClientKey:
+                handleClientKey(connectPacket, socketAddr);
                 break;
             case Log:
+                handleConnectLog(connectPacket, socketAddr);
                 break;
         }
     }
 
-    public void handleClientHello(ConnectPacket connectPacket, SocketAddress clientAddr) {
-        boolean successfulAdd = raft.addUser(connectPacket.username, clientAddr);
+    public void handleClientHello(ConnectPacket connectPacket, SocketAddress socketAddr) {
+        if (raft.raftMembershipState == RaftMembershipState.LEADER) {
+            boolean successfulAdd = raft.addUser(connectPacket.username, socketAddr);
+            if (successfulAdd) {
+                ConnectPacket responsePacket = new ConnectPacket(ConnectSubopcode.ServerHello, serverUsername, new byte[0]);
+                byte[] packetBytes = responsePacket.packetToBytes();
+                DatagramPacket datagramPacket = new DatagramPacket(packetBytes, packetBytes.length, socketAddr);
+                try {
+                    serverSocket.send(datagramPacket);
+                } catch (IOException e) {
+                    System.out.println("Unable to send datagram packet in method handleClientHello");
+                }
+            }
+        } else {
+            // TODO: send to leader
+        }
     }
 
-    public void handleCommandPakcet(Packet packet, SocketAddress clientAddr) {
+    public void handleServerHello(ConnectPacket connectPacket, SocketAddress socketAddr) {
+        if (!raft.raftSessionActive) {
+            raft.setLeader(connectPacket.username, new Session(socketAddr, System.nanoTime(), RaftMembershipState.LEADER));
+            ConnectPacket responsePacket = new ConnectPacket(ConnectSubopcode.ClientKey, serverUsername, new byte[0]);
+            byte[] packetBytes = responsePacket.packetToBytes();
+            DatagramPacket datagramPacket = new DatagramPacket(packetBytes, packetBytes.length, socketAddr);
+            try {
+                serverSocket.send(datagramPacket);
+            } catch (IOException e) {
+                System.out.println("Unable to send datagram packet in method handleServerHello");
+            }
+        }
+    }
+
+    public void handleClientKey(ConnectPacket connectPacket, SocketAddress socketAddr) {
+        if (raft.raftMembershipState == RaftMembershipState.LEADER) {
+            if (raft.userIsPending(connectPacket.username)) {
+                // add user to log
+                String command = RaftAdministrationCommand.ADD_MEMBER.name + " " + connectPacket.username + " " + System.nanoTime() + " " + socketAddr.toString().replace("/", "");
+                raft.addToRaftQueue(command);
+            }
+            // tell client to log that the raft session is active
+            ConnectPacket responsePacket = new ConnectPacket(ConnectSubopcode.Log, serverUsername, new byte[0]);
+            byte[] packetBytes = responsePacket.packetToBytes();
+            DatagramPacket datagramPacket = new DatagramPacket(packetBytes, packetBytes.length, socketAddr);
+            // TODO: start sending log
+        }
+    }
+
+    public void handleConnectLog(ConnectPacket connectPacket, SocketAddress socketAddr) {
+        if (!raft.raftSessionActive) {
+            raft.raftSessionActive = true;
+        }
+    }
+
+    public void handleCommandPakcet(Packet packet, SocketAddress socketAddr) {
         CommandPacket commandPacket = (CommandPacket) packet;
     }
 
-    public void handleLogPacket(Packet packet, SocketAddress clientAddr) {
+    public void handleLogPacket(Packet packet, SocketAddress socketAddr) {
         LogCommandPacket logCommandPacket = (LogCommandPacket) packet;
     }
 
-    public void handleHeartbeatPacket(Packet packet, SocketAddress clientAddr) {
-        HeartbeatPacket heartbeatPacket = (HeartbeatPacket) packet;
+    public void handleHeartbeatPacket(Packet packet, SocketAddress socketAddr) {
+        if (raft.addrIsLeader(socketAddr)) {
+            AckPacket ackPacket = new AckPacket(serverUsername);
+            byte[] packetBytes = ackPacket.packetToBytes();
+            DatagramPacket responsePacket = new DatagramPacket(packetBytes, packetBytes.length, socketAddr);
+            try {
+                serverSocket.send(responsePacket);
+            } catch (IOException e) {
+                System.out.println("Unable to send datagram packet in method handleHeartbeatPacket");
+            }
+        }
     }
 
-    public void handleAckPacket(Packet packet, SocketAddress clientAddr) {
+    public void handleAckPacket(Packet packet, SocketAddress socketAddr) {
         AckPacket ackPacket = (AckPacket) packet;
+        if (raft.raftMembershipState == RaftMembershipState.LEADER) {
+            // update last message received
+            raft.updateSessionTimeStamp(packet.username, socketAddr);
+            System.out.println("Ack received from Raft Follower.");
+        }
     }
 }
