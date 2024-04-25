06a63aa8de524fd89e7e418d4aac9acb8275d8eb
Victor-Lockwood <vlockwoo@oswego.edu>
2024-04-24 21:14:55 -0400

    Interim commit for the night

---
+++ src/main/java/edu/oswego/cs/gui/GuiMainTesting.java
@@ -0,0 +1,11 @@
+package edu.oswego.cs.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GuiMainTesting {
+
+    public static void main(String[] args) throws Exception {
+        MainFrame frame = new MainFrame();
+    }
+}
