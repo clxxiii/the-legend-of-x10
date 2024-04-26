6f2722bab2fcd3e68e8d6500c7f24107e88142d3
Victor-Lockwood <vlockwoo@oswego.edu>
2024-04-25 23:12:26 -0400

    I lied, I didn't go to bed.  Added comments.  Now good night.

--- src/main/java/edu/oswego/cs/gui/MainFrame.java
+++ src/main/java/edu/oswego/cs/gui/MainFrame.java
@@ -15,6 +15,7 @@ public class MainFrame extends JFrame {
         int borderSpacing = 10;
         String titleFontFamily = "Serif";
 
+        //The big cheese
         mainPanel = new JPanel(new BorderLayout());
         add(mainPanel);
 
@@ -29,13 +30,14 @@ public class MainFrame extends JFrame {
         JPanel outputPanel = new JPanel(new GridLayout(1, 2));
 
         //World text stuff - world events go here
-        JPanel worldTextPanel = new JPanel();
+        //TODO: Consider different font stuff.  Maybe color or something.
+        JPanel worldTextPanel = new JPanel();       //Just a container, really
         JTextArea outputText = new JTextArea("Output Text", 30, 40);
         outputText.setLineWrap(true);
         outputText.setEditable(false);
         worldTextPanel.add(outputText);
 
-        //Setup a scroll pane so we can, well, scroll.
+        //Set up a scroll pane so we can, well, scroll.  Stick the world panel into it
         JScrollPane worldTextScrollPane =
                 new JScrollPane(worldTextPanel,
                                 JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
@@ -43,7 +45,7 @@ public class MainFrame extends JFrame {
                 );
 
         //Map goes here
-        JPanel mapPanel = new JPanel();
+        JPanel mapPanel = new JPanel();     //Nother container
         JTextArea mapOutput = new JTextArea("Map Area", 30, 40);
         mapOutput.setEditable(false);
         mapPanel.add(mapOutput);
@@ -56,7 +58,6 @@ public class MainFrame extends JFrame {
         JPanel inputPanel = new JPanel(new BorderLayout());
 
         JTextField textField = new JTextField("Input");
-        textField.addActionListener(inputFieldAction);
         textField.setFont(new Font(textField.getFont().getName(), Font.ITALIC, textField.getFont().getSize()));
         inputPanel.setBorder(BorderFactory.createEmptyBorder(borderSpacing, borderSpacing, borderSpacing, borderSpacing));
         inputPanel.add(textField, BorderLayout.CENTER);
@@ -66,18 +67,24 @@ public class MainFrame extends JFrame {
         mainPanel.add(outputPanel, BorderLayout.CENTER);
         mainPanel.add(inputPanel, BorderLayout.SOUTH);
 
+        //This way the exit button actually exits :D
+        //TODO: Will likely need to perform an equivalent of the ".exit" command for raft
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        //I guess this sort of thing isn't recommended but might change my mind later
 //        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 //        double screenWidth = screenSize.getWidth();
 //        double screenHeight = screenSize.getHeight();
+//        setSize((int) (screenWidth / 2), (int) (screenHeight / 2));
 
-        //setSize((int) (screenWidth / 2), (int) (screenHeight / 2));
+        //Action listener stuff
+        textField.addActionListener(inputFieldAction);
 
-        setResizable(false);
-        pack();
-        setLocationRelativeTo(null);
-        setVisible(true);
+        //Final prep
+        setResizable(false);                //Looks gross maximized otherwise
+        pack();                             //Packs everything together to fit whatever size the components are
+        setLocationRelativeTo(null);        //Sticks the window in the middle
+        setVisible(true);                   //What it says on the tin
     }
 
     Action inputFieldAction = new AbstractAction() {
