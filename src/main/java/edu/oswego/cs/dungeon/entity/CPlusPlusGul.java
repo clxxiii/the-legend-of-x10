9f9fdcbc2f0431a9cac9a43c05aa2af0886f3ada
DeveloperDave17 <DavidHennigan17@gmail.com>
2024-05-02 21:25:20 -0400

    add more mobs

---
+++ src/main/java/edu/oswego/cs/dungeon/entity/CPlusPlusGul.java
@@ -0,0 +1,11 @@
+package edu.oswego.cs.dungeon.entity;
+
+import edu.oswego.cs.dungeon.Entity;
+
+public class CPlusPlusGul extends Entity {
+
+    public CPlusPlusGul() {
+        this.setHp(250);
+        this.name = "C++gul";
+    }
+}
---
+++ src/main/java/edu/oswego/cs/dungeon/entity/RoboCrab.java
@@ -0,0 +1,11 @@
+package edu.oswego.cs.dungeon.entity;
+
+import edu.oswego.cs.dungeon.Entity;
+
+public class RoboCrab extends Entity {
+
+    public RoboCrab() {
+        this.setHp(125);
+        this.name = "RoboCrab";
+    }
+}
